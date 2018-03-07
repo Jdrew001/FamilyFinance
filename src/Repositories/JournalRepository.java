dbaf052444196616fce0f3cb11ca0e6a968bb411
drew <dtatkison@gmail.com>
2018-03-07 09:07:08 -0600

    got started on journal repo

---
+++ src/Repositories/JournalRepository.java
@@ -0,0 +1,22 @@
+package Repositories;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class JournalRepository extends BaseRepository {
+
+    ObservableList<Income> incomes = FXCollections.observableArrayList();
+
+    //get all journal entries
+
+    //get all by month
+
+    //get one by id
+
+    //update journal entry
+
+    //add journal entry
+
+    //delete journal entry
+
+}
